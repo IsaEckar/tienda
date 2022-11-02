667200c6ed605ab274c66dfa5bc81390de0da5bf
Isaura <110794045+IsaEckar@users.noreply.github.com>
2022-11-01 22:04:31 -0500

    archivos

--- front/src/App.js
+++ front/src/App.js
@@ -7,9 +7,9 @@ import { ProductDetails } from './components/products/ProductDetails';
 //Router traido desde react-router-dom (no confundir con el de express)
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/admin/Dashboard';
-import ProductsList from './components/admin/ProductsList';
 import NewProduct from './components/admin/newProduct';
 import Cart from './components/cart/Cart';
+import ProductsList from './components/admin/ProductsList';
 
 
 function App() {
